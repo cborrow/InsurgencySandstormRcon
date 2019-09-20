63510dd515d2786f3aee4324154ad000af499c64
Cory Borrow <cborrow03@gmail.com>
2019-09-20 11:13:30 -0400

    Removed unsued file from resources

--- InsurgencySandstormRcon/Properties/Resources.Designer.cs
+++ InsurgencySandstormRcon/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace InsurgencySandstormRcon.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] RconServers {
-            get {
-                object obj = ResourceManager.GetObject("RconServers", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
