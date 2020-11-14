baf28a9fad321ce57ea0730b1dc7ce4d74dbb2b5
Cory Borrow <cborrow03@gmail.com>
2020-11-13 20:38:40 -0500

    Generate banned user list on BanListDialog load.  - Get list of banned users.  - Parse list of users to split users, then user info.  - Fill ListBox with ability to select and unban user.

--- InsurgencySandstormRcon/Dialogs/BanListDialog.cs
+++ InsurgencySandstormRcon/Dialogs/BanListDialog.cs
@@ -17,9 +17,53 @@ namespace InsurgencySandstormRcon
             InitializeComponent();
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            listBox1.Items.Clear();
+            if(RconServerManager.Instance.ActiveServer != null)
+            {
+                string result = RconServerManager.Instance.ActiveServer.Rcon.SendCommand("listbans");
+                ParseBanListString(result);
+            }
+        }
+
+        protected void ParseBanListString(string banlist)
+        {
+            string[] bannedUsers = banlist.Split(')');
+
+            foreach(string banEntry in bannedUsers)
+            {
+                if (string.IsNullOrEmpty(banEntry))
+                    continue;
+
+                string[] parts = banEntry.Split(' ');
+
+                if (parts.Length > 1 && banEntry.Contains("("))
+                {
+                    string user = parts[0];
+                    string dateStr = parts[1];
+                    DateTime dt = DateTime.Now;
+
+                    if (DateTime.TryParse(parts[1], out dt))
+                    {
+                        dateStr = dt.ToString();
+                    }
+
+                    int reasonIndex = banEntry.IndexOf("(", 20) + 1;
+
+                    string reason = banEntry.Substring(reasonIndex, (banEntry.Length - reasonIndex));
+
+                    listBox1.Items.Add(string.Format("{0} on {1} | {2}", user, dateStr, reason));
+                }
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             string selectedPlayer = (string)listBox1.SelectedItem;
+            string id = selectedPlayer.Split(' ')[0]; //Find a better way to get ID
 
             if(selectedPlayer != null)
             {
